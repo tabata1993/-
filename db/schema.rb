3816ef8bc5b8c6d8471346f69564ebc54100d30c
田畑智士 <tabatasatoshi1993@gmail.com>
2018-07-21 18:10:32 +0900

    dbをpostgrsqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180718061459) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.integer "group_id"
     t.integer "user_id"
